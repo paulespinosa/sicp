75e55a02765fe9b93d1527d94c00b268a85f0edf
Mr. Paul <mrpaul@aestheticwisdom.com>
2023-03-02 20:22:16 +0700

    Exercise 2.33 Fill in the Blanks
    Three common list-manipulation operations implemented using accumulate.

---
+++ ex2.33/sequence.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y))
+              nil sequence))
+
+(map (lambda (x) (* x 2)) (list 1 2 3 4 5))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append (list 1 2 3) (list 4 5 6))
+(append (list 1 2 3) nil)
+(append nil (list 4 5 6))
+
+(define (length sequence)
+  (accumulate (lambda (_ y) (+ 1 y)) 0 sequence))
+
+(length (list 1))
+(length (list 1 2 3 4))
+(length (list 1 2 3 4 5))
