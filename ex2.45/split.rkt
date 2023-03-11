697a38e2717df55d3194874716b845b0f20fd420
Mr. Paul <mrpaul@aestheticwisdom.com>
2023-03-11 19:44:04 +0700

    Exercise 2.45 split

---
+++ ex2.45/split.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+;; (define (right-split painter n)
+;;   (if (= n 0)
+;;       painter
+;;       (let ((smaller (right-split painter 
+;;                                   (- n 1))))
+;;         (beside painter 
+;;                 (below smaller smaller)))))
+
+(define (split op1 op2)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller ((split op1 op2) painter (- n 1))))
+          (op1 painter (op2 smaller smaller))))))
+
+;; (define right-split (split beside below))
+;; (define up-split (split below beside))
