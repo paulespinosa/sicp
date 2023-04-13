aac7248668a66ed847d9cb7f7669bc1963131624
Mr. Paul <mrpaul@aestheticwisdom.com>
2023-04-13 15:43:41 +0700

    An subset exercise: canEqualTarget
    Given a list of numbers and a target number, can any combination of numbers sum
    to the target number?
    
    I added one solution to this problem in the existing exercise 2.32.

--- ex2.32/subsets.rkt
+++ ex2.32/subsets.rkt
@@ -7,6 +7,26 @@
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (e) (cons (car s) e)) rest)))))
 
-(subsets (list 1 2 3))
-(subsets (list 1 2 3 4))
+(length (subsets (list 1 2 3)))
+(length (subsets (list 1 2 3 4)) )
+(length (subsets (list 1 2 3 4 5 6 7 8 9 10)) )
 
+;; (define (accumulate op initial sequence)
+;;   (if (null? sequence)
+;;       initial
+;;       (op (car sequence)
+;;           (accumulate op
+;;                       initial
+;;                       (cdr sequence)))))
+
+;; (define (canEqualTarget elements T)
+;;   (accumulate (lambda (head rest) (or rest (= T (accumulate + 0 head) )))
+;;               #f
+;;               (subsets elements)))
+
+;; (canEqualTarget (list 2 4 8) 10) ; t
+;; (canEqualTarget (list 2 4 8) 12) ; t
+;; (canEqualTarget (list -2 4 8 18 3) 24) ; t
+;; (canEqualTarget (list 2 4 8 6) 11) ; f
+;; (canEqualTarget '() 0) ; t
+;; (canEqualTarget (list 1 -5 3) 4) ; t
